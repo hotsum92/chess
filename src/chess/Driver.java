48bbe809562580118b63f842feb3c9311f236d6a
hotsuma kawashima <hkawashimajob1992@gmail.com>
2019-11-07 14:47:54 -0800

    add public in main method

--- src/chess/Driver.java
+++ src/chess/Driver.java
@@ -2,7 +2,7 @@ package chess;
 
 public class Driver {
 
-    static void main(String[] args) {
+     public static void main(String[] args) {
         var game = new Game();
         System.out.println(game);
     }
