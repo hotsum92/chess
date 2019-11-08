90f850fc01e12cae74c64856616d08e08ebbc0d9
Yuka <yuka.0531.a@gmail.com>
2019-11-08 15:33:05 -0800

    update Knight and Queen

--- src/chess/Knight.java
+++ src/chess/Knight.java
@@ -8,7 +8,26 @@ class Knight extends Piece {
 
     @Override
     boolean move(Position newPosition) {
+        if(!super.isValidMove(newPosition)) {
+            return false;
+        }
+
+        if(canMoveInLShape(newPosition)) {
+
+            game.remove(position);
+            game.replace(newPosition, this);
+
+            position = newPosition;
+            return true;
+        }
         return false;
+    }
+
+    boolean canMoveInLShape(Position newPosition) {
+        var piece = game.pieceAt(newPosition);
+        return (position.horizontalDistanceTo(newPosition) == 2 && position.verticalDistanceTo(newPosition) == 1)
+                || (position.horizontalDistanceTo(newPosition) == 1 && position.verticalDistanceTo(newPosition) == 2)
+                && (game.pieceAt(newPosition).isEmpty || !owner.has(piece));
     }
 
     @Override
--- src/chess/Queen.java
+++ src/chess/Queen.java
@@ -7,6 +7,46 @@ class Queen extends Piece {
     }
 
     @Override
+    boolean move(Position newPosition) {
+        if(!super.isValidMove(newPosition)) {
+            return false;
+        }
+
+        if(canMoveVertically(newPosition)
+                || canMoveHorizontally(newPosition)
+                || canMoveDiagonally(newPosition)) {
+
+            game.remove(position);
+            game.replace(newPosition, this);
+
+            position = newPosition;
+            return true;
+        }
+        return false;
+    }
+
+    boolean canMoveVertically(Position newPosition) {
+        var piece = game.pieceAt(newPosition);
+        return newPosition.isVetical(position)
+                && !game.pieceExistsBetween(position, newPosition)
+                && (game.pieceAt(newPosition).isEmpty || !owner.has(piece));
+    }
+
+    boolean canMoveHorizontally(Position newPosition) {
+        var piece = game.pieceAt(newPosition);
+        return newPosition.isHotizontal(position)
+                && !game.pieceExistsBetween(position, newPosition)
+                && (game.pieceAt(newPosition).isEmpty || !owner.has(piece));
+    }
+
+    boolean canMoveDiagonally(Position newPosition) {
+        var piece = game.pieceAt(newPosition);
+        return newPosition.isDiagonal(position)
+                && !game.pieceExistsBetween(position, newPosition)
+                && (game.pieceAt(newPosition).isEmpty || !owner.has(piece));
+    }
+
+    @Override
     public String toString() {
         if(owner == game.white) {
             return "♕";
