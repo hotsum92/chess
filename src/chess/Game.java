31212b9a6f1bcaeee308f0a5a335a480cdfcec05
hotsuma kawashima <hkawashimajob1992@gmail.com>
2019-11-10 14:49:08 -0800

    move start functiont from driver to game

--- src/chess/Game.java
+++ src/chess/Game.java
@@ -1,14 +1,138 @@
 package chess;
 
+import java.util.*;
+
+import static chess.GameTestHelper.*;
+
 class Game {
     static final Player white = new Player("White");
     static final Player black = new Player("Black");
     static final Player[] players = new Player[] { white, black };
 
     Board board;
+    Display display;
 
     Game() {
         board = new Board();
+        display = new Display();
+    }
+
+    void start() {
+        while (true) {
+            display.show(board);
+
+            while (true) {
+                display.showNewLine();
+                var command = display.askCommand();
+
+                if("help".equals(command)) {
+                    display.showHelp();
+                    continue;
+                }
+
+                if("board".equals(command)) {
+                    break;
+                }
+
+                if("resign".equals(command)) {
+                    resign();
+                    display.showResultOf(winner());
+                    return;
+                }
+
+                if("moves".equals(command)) {
+                    display.showAllPossibleMovementsOf(board);
+                    continue;
+                }
+
+                if("test".equals(command)) {
+                    StartTestConsole();
+                    break;
+                }
+
+                if(command.length() == 2) {
+                    var from = new UCI(command.substring(0, 2));
+                    if(!from.isValid()) {
+                        display.showInvalidInputError();
+                        continue;
+                    }
+                    var piece = board.pieceAt(from.toPosition());
+                    display.showAllPossibleMovementsOf(piece);
+                    continue;
+                }
+
+                if(command.length() == 4) {
+                    var from = new UCI(command.substring(0, 2));
+                    var to   = new UCI(command.substring(2, 4));
+                    if(!(from.isValid() && to.isValid())) {
+                        display.showInvalidInputError();
+                        continue;
+                    }
+                    if(!move(from.toPosition(), to.toPosition())) {
+                        display.showInvalidInputError();
+                        continue;
+                    }
+                    if(judgeWin()) {
+                        display.showResultOf(winner());
+                        return;
+                    }
+                    togglePlayer();
+                    break;
+                }
+
+                display.showInvalidInputError();
+            }
+        }
+    }
+
+    void StartTestConsole() {
+        var board = emptyBoard();
+
+        var str = display.askPiece();
+
+        var hash = new HashMap<String, Piece>();
+        hash.put("1", new King(center, white, board));
+        hash.put("2", new Rook(center, white, board));
+        hash.put("3", new Bishop(center, white, board));
+        hash.put("4", new Queen(center, white, board));
+        hash.put("5", new Knight(center, white, board));
+        hash.put("6", new Pawn(center, white, board));
+
+        if(!hash.containsKey(str)) return;
+
+        board.pieces[center.row][center.col] = hash.get(str);
+        board.select(center);
+
+        while (true) {
+            display.show(board);
+
+            while (true) {
+                var command = display.askTestCommand();
+
+                if("board".equals(command)) {
+                    break;
+                }
+
+                if("moves".equals(command)) {
+                    display.showAllPossibleMovementsOf(board);
+                }
+
+                if("quit".equals(command)) {
+                    return;
+                }
+
+                if(command.length() == 2) {
+                    var to = new UCI(command.substring(0, 2), 3);
+                    if(!board.move(to.toPosition())) {
+                        display.showInvalidInputError();
+                    }
+                    board.select(to.toPosition());
+                    break;
+                }
+
+                display.showInvalidInputError();
+            }
+        }
     }
 
     boolean move(Position from, Position to) {
