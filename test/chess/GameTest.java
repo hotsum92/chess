aeaea218cd4b4be628e0976e74d2c54b73b54d29
Yuka <yuka.0531.a@gmail.com>
2019-11-13 13:14:46 -0800

    add GameTest.java for testing Game class

---
+++ test/chess/GameTest.java
@@ -0,0 +1,32 @@
+package chess;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void start() {
+        MockConsole mock = new MockConsole();
+        Display display = new Display(mock);
+        Game game = new Game(display);
+
+        mock.inputList= new ArrayList<>();
+        mock.inputList.add("a2a3");
+        mock.inputList.add("resign");
+
+        game.start();
+
+        var originalPositionPiece = game.board.pieceAt(new Position(6, 0)); // Should be "Empty"
+        var newPositionPiece = game.board.pieceAt(new Position(5, 0)); // Should be "Pawn"
+
+        assertEquals("・", originalPositionPiece.toString());
+        assertEquals("♙", newPositionPiece.toString());
+        assertEquals("resign", mock.inputList.get(1));
+    }
+}
