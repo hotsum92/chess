b6fe7abf768af6c4bd985f6d4a664bdffd46a6a7
elencrisf <elen.crisf@uol.com.br>
2019-11-12 20:32:53 -0800

    let me check 2nd

--- src/chess/King.java
+++ src/chess/King.java
@@ -8,6 +8,36 @@ class King extends Piece {
         super(position, owner, board);
     }
 
+
+    @Override
+    boolean canMoveTo(Position newPosition) {
+        if(!super.canMoveTo(newPosition)) {
+            return false;
+        }
+
+        // TODO: implement castling
+
+        return canMoveVertically(newPosition)
+                || canMoveHorizontally(newPosition);
+    }
+
+    boolean canMoveVertically(Position newPosition) {
+        var piece = board.pieceAt(newPosition);
+        return position.isVetical(newPosition)
+                && position.verticalDistanceTo(newPosition) == 1
+                && !board.pieceExistsBetween(position, newPosition)
+                && (board.pieceAt(newPosition).isEmpty || !owner.has(piece));
+    }
+
+    boolean canMoveHorizontally(Position newPosition) {
+        var piece = board.pieceAt(newPosition);
+        return position.isHotizontal(newPosition)
+                && position.horizontalDistanceTo(newPosition) == 1
+                && !board.pieceExistsBetween(position, newPosition)
+                && (board.pieceAt(newPosition).isEmpty || !owner.has(piece));
+    }
+
+
     @Override
     public String toString() {
         if(owner == white) {
--- src/chess/Rook.java
+++ src/chess/Rook.java
@@ -9,6 +9,31 @@ class Rook extends Piece {
     }
 
     @Override
+    boolean canMoveTo(Position newPosition) {
+        if(!super.canMoveTo(newPosition)) {
+            return false;
+        }
+
+        return canMoveVertically(newPosition)
+                || canMoveHorizontally(newPosition);
+    }
+
+    boolean canMoveVertically(Position newPosition) {
+        var piece = board.pieceAt(newPosition);
+        return position.isVetical(newPosition)
+                && !board.pieceExistsBetween(position, newPosition)
+                && (board.pieceAt(newPosition).isEmpty || !owner.has(piece));
+    }
+
+    boolean canMoveHorizontally(Position newPosition) {
+        var piece = board.pieceAt(newPosition);
+        return position.isHotizontal(newPosition)
+                && !board.pieceExistsBetween(position, newPosition)
+                && (board.pieceAt(newPosition).isEmpty || !owner.has(piece));
+    }
+
+
+    @Override
     public String toString() {
         if(owner == white) {
             return "♖";
